db28139aad93221461ecdda9be1a7ff5e9615246
alessandroverao <nk9mhpblu@gmail.com>
2023-04-08 09:06:46 -0300

    update 08.04.2023 09.06hs

--- js/funcionesCotizacion.js
+++ js/funcionesCotizacion.js
@@ -1,3 +1,24 @@
+// input producto cambiar imagen icon segun lo seleccionado 
+// Oculta inicialmente ambas imágenes
+document.getElementById("cereza-icon").style.display = "none";
+document.getElementById("arandano-icon").style.display = "none";
+
+// Función que se ejecuta cuando el usuario cambia la opción seleccionada
+function updateProductIcon() {
+    // Obtiene el valor de la opción seleccionada
+    var selectedProduct = document.getElementById("select-producto").value;
+
+    // Muestra la imagen correspondiente y oculta la otra
+    if (selectedProduct === "cereza") {
+        document.getElementById("cereza-icon").style.display = "inline";
+        document.getElementById("arandano-icon").style.display = "none";
+        document.getElementById("frutafina-icon").style.display = "none";
+    } else if (selectedProduct === "arandano") {
+        document.getElementById("cereza-icon").style.display = "none";
+        document.getElementById("frutafina-icon").style.display = "none";
+        document.getElementById("arandano-icon").style.display = "inline";
+    }
+}
 // generar variedades apartir del producto seleccionado 
 const selectProducto = document.querySelector('#select-producto');
 const selectVariedad = document.querySelector('#select-variedad');
@@ -24,43 +45,43 @@ selectProducto.addEventListener('change', (event) => {
 });
 // agregar un precio a cada variedad de cereza y de arandano
 const preciosCereza = {
-    'Santina': 1639,
-    'Van': 1543,
-    'Bing': 1927,
-    'Stella': 1803,
-    'Lapins': 1919,
-    'Kordia': 1535,
-    'Regina': 1438,
-    'Sweet Heart': 1401,
-    'Burlat': 1050,
-    'Marvin': 1622,
-    'Royal Tioga': 1852,
-    'Pacific Red': 1422,
-    'Ruby': 1759,
-    'Early Red': 1705,
-    'Royal Bailey': 1677,
-    'Royal Hazel': 1370,
-    'Rocket': 1581,
-    'Carmen': 1477,
-    'Frisco': 1648,
-    'Brocks': 1693
+    'Santina': 639,
+    'Van': 543,
+    'Bing': 927,
+    'Stella': 803,
+    'Lapins': 919,
+    'Kordia': 535,
+    'Regina': 438,
+    'Sweet Heart': 401,
+    'Burlat': 050,
+    'Marvin': 622,
+    'Royal Tioga': 852,
+    'Pacific Red': 422,
+    'Ruby': 759,
+    'Early Red': 705,
+    'Royal Bailey': 677,
+    'Royal Hazel': 370,
+    'Rocket': 581,
+    'Carmen': 477,
+    'Frisco': 648,
+    'Brocks': 693
 };
 const preciosArandano = {
-    'Duke': 1391,
-    'Draper': 1357,
-    'Blue Ribbon': 1308,
-    'Blue Crop': 1789,
-    'Liberty': 1091,
-    'Blue Gold': 1567,
-    'Top Shelf': 1690,
-    'Aurora': 1487,
-    'Last Call': 1898,
-    'New Hanover': 1888,
-    'Alixblue': 1453,
-    'Gupton': 1822,
-    'Ozarkblue®': 1393,
-    'Star': 1197,
-    'Colibrí': 1981
+    'Duke': 391,
+    'Draper': 357,
+    'Blue Ribbon': 308,
+    'Blue Crop': 789,
+    'Liberty': 091,
+    'Blue Gold': 567,
+    'Top Shelf': 690,
+    'Aurora': 487,
+    'Last Call': 898,
+    'New Hanover': 888,
+    'Alixblue': 453,
+    'Gupton': 822,
+    'Ozarkblue®': 393,
+    'Star': 197,
+    'Colibrí': 981
 };
 // generar tipo de caja apartir del producto selecionado
 const selectTipoCaja = document.querySelector('#select-tipo-caja');
@@ -83,23 +104,45 @@ selectProducto.addEventListener('change', () => {
 });
 // agregar un precio a cada tipo de caja
 const preciosCajaArandano = {
-    '2,5kg': 358,
-    '5kg': 501.25,
-    '7,5kg': 753
+    '2,5kg': 38,
+    '5kg': 51.25,
+    '7,5kg': 75
 };
 const preciosCajaCereza = {
-    '2,5kg - Tapa Blanca': 350,
-    '2,5kg - Tapa Roja': 353.29,
-    '2,5kg - Tapa Marrón': 310,
-    '5kg - Tapa Blanca': 515,
-    '5kg - Tapa Roja': 518,
-    '5kg - Tapa Marrón': 499.47
+    '2,5kg - Tapa Blanca': 5,
+    '2,5kg - Tapa Roja': 3.29,
+    '2,5kg - Tapa Marrón': 10,
+    '5kg - Tapa Blanca': 15,
+    '5kg - Tapa Roja': 18,
+    '5kg - Tapa Marrón': 9.47
+};
+// agregar kg a cada tipo de caja
+const kgCajaArandano = {
+    '2,5kg': 2.5,
+    '5kg': 5,
+    '7,5kg': 7.5
+};
+const kgCajaCereza = {
+    '2,5kg - Tapa Blanca': 2.5,
+    '2,5kg - Tapa Roja': 2.5,
+    '2,5kg - Tapa Marrón': 2.5,
+    '5kg - Tapa Blanca': 5,
+    '5kg - Tapa Roja': 5,
+    '5kg - Tapa Marrón': 5
 };
 //validacion de input number al ingresar un valor
 const inputCantidadCajas = document.querySelector('#cantidad-cajas');
 const errorMessage = document.querySelector('#error-message');
+// Agrega un controlador de eventos para el evento keydown
+inputCantidadCajas.addEventListener('keydown', (event) => {
+    // Verifica si la tecla presionada es un punto o una coma
+    if (event.key === '.' || event.key === ',') {
+        // Cancela el evento para evitar que se ingrese el punto o la coma
+        event.preventDefault();
+    }
+});
 inputCantidadCajas.addEventListener('input', () => {
-    const value = inputCantidadCajas.value;
+    let value = inputCantidadCajas.value;
     if (value === '' || !Number.isInteger(Number(value)) || Number(value) <= 0) {
         // El valor ingresado no es válido
         // Mostrar un mensaje de error al usuario
@@ -117,15 +160,54 @@ document.querySelector('.btn-success').addEventListener('click', function () {
     let variedad = document.querySelector('#select-variedad').value;
     let tipoCaja = document.querySelector('#select-tipo-caja').value;
     let cantidadCajas = document.querySelector('#cantidad-cajas').value;
+
+    let totalProducto = 0;
+    let totalCajas = 0;
+    let totalKg = 0;
+    let total = 0;
+
     if (producto !== "Seleccione un producto" && variedad !== "Seleccione un tipo de variedad" && tipoCaja !== "Seleccione un tipo de caja"
         && cantidadCajas !== "" && Number.isInteger(Number(cantidadCajas)) && Number(cantidadCajas) > 0) {
         document.querySelector('.btn-danger').style.display = 'inline';
         document.querySelector('.btn-primary').style.display = 'inline';
         document.querySelectorAll('input:not(.btn-danger, .btn-primary):not(#modalContacto input), select:not(#modalContacto select)').forEach(function (el) {
             el.disabled = true;
+            el.classList.remove('input-error');
         });
         this.disabled = true;
         document.querySelector('#error-message').style.display = 'none';
+        // Actualiza el contenido del elemento de resumen
+        // Convierte el valor a un número
+        let cantidadCajasParaMostrar = Number(cantidadCajas);
+
+        if (producto === 'cereza') {
+            totalKg = kgCajaCereza[tipoCaja] * cantidadCajas;
+            totalProducto = preciosCereza[variedad] * totalKg;
+            totalCajas = preciosCajaCereza[tipoCaja] * cantidadCajas;
+            total = totalProducto + totalCajas;
+
+            document.getElementById("resumen").innerHTML = `El precio por kg de la variedad ${variedad} de ${producto}s es $${preciosCereza[variedad]} 
+            y el tipo de caja es "${tipoCaja}", su precio por unidad es $${preciosCajaCereza[tipoCaja]}, fueron elegidas ${cantidadCajasParaMostrar.toLocaleString('de-DE')} caja/s. <br>
+            Total kg: ${totalKg.toLocaleString('de-DE')} <br>
+            Total Producto: $${totalProducto.toLocaleString('de-DE')} <br>
+            Total Cajas: $${totalCajas.toLocaleString('de-DE')} <br>
+            Total a Pagar: $${total.toLocaleString('de-DE')}`;
+
+        } else if (producto === 'arandano') {
+            totalKg = kgCajaArandano[tipoCaja] * cantidadCajas;
+            totalProducto = preciosArandano[variedad] * totalKg;
+            totalCajas = preciosCajaArandano[tipoCaja] * cantidadCajas;
+            total = totalProducto + totalCajas;
+
+            document.getElementById("resumen").innerHTML = `El precio por kg de la variedad ${variedad} de ${producto}s es $${preciosArandano[variedad]} 
+            y el tipo de caja es ${tipoCaja}, su precio por unidad es $${preciosCajaArandano[tipoCaja]}, fueron elegidas ${cantidadCajasParaMostrar.toLocaleString('de-DE')} caja/s. <br>
+            Total kg: ${totalKg.toLocaleString('de-DE')} <br>
+            Total Producto: $${totalProducto.toLocaleString('de-DE')} <br>
+            Total Cajas: $${totalCajas.toLocaleString('de-DE')} <br>
+            Total a Pagar: $${total.toLocaleString('de-DE')}`;
+
+        }
+
     } else {
         document.querySelector('#error-message').textContent = 'Por favor complete todos los campos';
         document.querySelector('#error-message').style.display = 'block';
@@ -135,6 +217,19 @@ document.querySelector('.btn-success').addEventListener('click', function () {
         setTimeout(() => {
             errorMessage.classList.remove('highlight');
         }, 1000);
+        // Agregar la clase "input-error" a los elementos de entrada que no se han completado correctamente
+        if (producto === "Seleccione un producto") {
+            document.querySelector('#select-producto').classList.add('input-error');
+        }
+        if (variedad === "Seleccione un tipo de variedad") {
+            document.querySelector('#select-variedad').classList.add('input-error');
+        }
+        if (tipoCaja === "Seleccione un tipo de caja") {
+            document.querySelector('#select-tipo-caja').classList.add('input-error');
+        }
+        if (cantidadCajas === "" || !Number.isInteger(Number(cantidadCajas)) || Number(cantidadCajas) <= 0) {
+            document.querySelector('#cantidad-cajas').classList.add('input-error');
+        }
     }
 });
 // Agregar un controlador de eventos al nuevo botón "Nueva cotización"
@@ -142,6 +237,7 @@ document.querySelector('.btn-primary').addEventListener('click', function () {
     // Desbloquear los campos de entrada y selección
     document.querySelectorAll('input:not(.btn-danger, .btn-primary):not(#modalContacto input), select:not(#modalContacto select)').forEach(function (el) {
         el.disabled = false;
+        el.classList.remove('input-error');
     });
     // Mostrar los botones ocultos
     document.querySelector('.btn-success').style.display = 'inline';
@@ -150,3 +246,22 @@ document.querySelector('.btn-primary').addEventListener('click', function () {
     // Ocultar el botón "btn-danger"
     document.querySelector('.btn-danger').style.display = 'none';
 });
+//controlar el input si ya fue seleccionado un item luego del error al presionar el boton
+document.querySelectorAll('input:not(.btn-danger, .btn-primary):not(#modalContacto input), select:not(#modalContacto select)').forEach(function (el) {
+    el.addEventListener('input', function () {
+        // Verificar si el contenido del elemento de entrada es válido
+        if (this.value !== "") {
+            // Eliminar la clase "input-error" del elemento
+            this.classList.remove('input-error');
+        }
+    });
+});
+// evento que vacia los input del form en el caso que se avance una pagina o si se retrocede una pagina y vuelva a la misma luego.
+window.addEventListener('beforeunload', function (event) {
+    // Limpiar el contenido del formulario
+    document.querySelectorAll('input, select').forEach(function (el) {
+        el.value = '';
+    });
+});
+
+
--- js/funcionesNosotros.js
+++ js/funcionesNosotros.js
@@ -1,11 +1,9 @@
 // Obtener el boton
 let mybutton = document.getElementById("btn-back-to-top");
-
 // Cuando el usuario se desplaza hacia abajo 20px desde la parte superior del documento, muestra el botón
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   if (
     document.body.scrollTop > 20 ||
@@ -18,20 +16,14 @@ function scrollFunction() {
 }
 // Cuando el usuario haga clic en el botón, desplácese hasta la parte superior del documento
 mybutton.addEventListener("click", backToTop);
-
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
-
-
 // evitar cierre modal al hacer click boton submit
 $("form").submit(function (event) {
   event.preventDefault();
 });
-
 // el boton con el texto aparece oculto y desactivado en el modal y mostrarlo cuando se haga click en el boton enviar (submit), vaciar los input
 document.querySelector('#modalContacto').addEventListener('submit', function (event) {
   event.preventDefault();
@@ -53,4 +45,89 @@ inputs.forEach(input => {
   input.addEventListener('focus', () => {
     document.querySelector('#btn-oculto-modal').style.display = 'none';
   });
+});
+// validar input nombre que solo se ingresen letras y espacios y cantidad de letras ingresadas
+let inputNombre = document.querySelector('#nombre');
+let regex = /^[a-zA-Z\s]*$/;
+let minLength = 5;
+let maxLength = 100;
+
+inputNombre.addEventListener('input', function () {
+  let words = inputNombre.value.trim().split(/\s+/);
+  if (!regex.test(inputNombre.value)) {
+    inputNombre.setCustomValidity('Por favor ingresa solo letras y espacios.');
+  } else if (words.length !== 2) {
+    inputNombre.setCustomValidity('Por favor ingresa nombre y apellido.');
+  } else if (words.some(word => word.length < minLength)) {
+    inputNombre.setCustomValidity(`Cada palabra debe tener al menos ${minLength} caracteres.`);
+  } else if (words.some(word => word.length > maxLength)) {
+    inputNombre.setCustomValidity(`Cada palabra no debe tener más de ${maxLength} caracteres.`);
+  } else {
+    inputNombre.setCustomValidity('');
+  }
+});
+// validar input asunto cantidad de letras ingresadas 
+let inputAsunto = document.querySelector('#asunto');
+inputAsunto.addEventListener('input', function () {
+  if (inputAsunto.value.length < minLength) {
+    inputAsunto.setCustomValidity(`El asunto debe tener al menos ${minLength} caracteres.`);
+  } else if (inputAsunto.value.length > maxLength) {
+    inputAsunto.setCustomValidity(`El asunto no debe tener más de ${maxLength} caracteres.`);
+  } else {
+    inputAsunto.setCustomValidity('');
+  }
+});
+// validar input email - y consumo de api que verifica la existencia del email
+let inputEmail = document.querySelector('#email');
+let regexDos = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let allowedDomains = [
+  'gmail.com',
+  'hotmail.com',
+  'yahoo.com',
+  'outlook.com',
+  'icloud.com',
+  'aol.com',
+  'msn.com',
+  'live.com',
+  'yandex.com',
+  'protonmail.com'
+];
+let apiKey = 'fdac890a56950b1b05045bb709b8623e4cd8ce30'; /// codigo key de mi cuenta creada en hunter
+inputEmail.addEventListener('input', function () {
+  if (!regexDos.test(inputEmail.value)) {
+    inputEmail.setCustomValidity('Por favor ingresa un correo electrónico válido.');
+  } else {
+    let domain = inputEmail.value.split('@')[1];
+    if (!allowedDomains.includes(domain)) {
+      inputEmail.setCustomValidity(`El dominio ${domain} no está permitido.`);
+    } else {
+      fetch(`https://api.hunter.io/v2/email-verifier?email=${inputEmail.value}&api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+          // Procesar los datos recibidos de la API
+          if (data.data.status === 'invalid') {
+            inputEmail.setCustomValidity('El correo electrónico no es válido o no existe.');
+          } else {
+            inputEmail.setCustomValidity('');
+          }
+        });
+    }
+  }
+});
+// validar input mensaje - validacion min y maximo de caracteres
+inputMensaje = document.querySelector('#mensaje');
+let minLengthDos = 4;
+let maxLengthDos = 500;
+let malasPalabras = ['lrpmqtphdrmp', 'river plate', 'river', 'trabajar el fin de semana', 'culo'];
+
+inputMensaje.addEventListener('input', function () {
+  if (inputMensaje.value.length < minLengthDos) {
+    inputMensaje.setCustomValidity(`El mensaje debe tener al menos ${minLengthDos} caracteres.`);
+  } else if (inputMensaje.value.length > maxLengthDos) {
+    inputMensaje.setCustomValidity(`El mensaje no debe tener más de ${maxLengthDos} caracteres.`);
+  } else if (malasPalabras.some(word => inputMensaje.value.toLowerCase().includes(word))) {
+    inputMensaje.setCustomValidity('El mensaje contiene palabras o frases inapropiadas o prohibidas.');
+  } else {
+    inputMensaje.setCustomValidity('');
+  }
 });
